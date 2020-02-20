c915714ec58d0846ae9a566b70562a0595670c87
Egereva Marina <marina.egereva@gmail.com>
2020-02-20 19:00:03 +0300

    first commit

---
+++ src/pages/NotifyPage.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+
+        <!-- wrapper -->
+        <div class="notify__wrapper">
+
+          <!-- title -->
+          <div class="notify-title">
+            <p>Notify App</p>
+            <!-- svg-->
+          </div>
+
+          <!-- notify box-->
+          <div class="notify__content">
+
+          <!-- preloader -->
+            <preloader v-if="loading" :width="90" :height="90"/>
+
+          <!-- notify -->
+            <notify v-if="!loading"
+              :messages = "messages"/>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import notify from '@/components/Notify.vue'
+  //UI
+  import preloader from '@/components/UI/Preloader.vue'
+
+  export  default {
+    components: {notify, preloader},
+    data () {
+      return {
+        loading: false,
+        messages: [
+          { title: 'message 1'},
+          { title: 'message 2'},
+          { title: 'message 3'},
+          { title: 'message 4'},
+          { title: 'message 5'},
+          { title: 'message 6'},
+        ]
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 90vh;
+  }
+
+  .notify__wrapper {
+    width: 400px;
+    background: #fff;
+    padding:  30px;
+    border-radius: 16px;
+    box-shadow: 0 12px 22px 0 rgba(0,0,0,0.1);
+  }
+
+  .notify__content {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    min-height: 300px;
+  }
+
+  .notify-title {
+    p {
+      font-size: 24px;
+    }
+  }
+
+</style>
